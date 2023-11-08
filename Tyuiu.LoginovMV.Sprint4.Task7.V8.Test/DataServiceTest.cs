8ac2975258702455995d62f9361fcb909cdefdbe
Максим Логинов <maxdompc@gmail.com>
2023-11-08 14:39:22 +0500

    Task7

---
+++ Tyuiu.LoginovMV.Sprint4.Task7.V8.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LoginovMV.Sprint4.Task7.V8.Lib;
+
+namespace Tyuiu.LoginovMV.Sprint4.Task7.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int n = 3;
+            int m = 4;
+            string value = "264795863157";
+            int res = ds.Calculate(n, m, value);
+            int wait = 37;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.LoginovMV.Sprint4.Task7.V8/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LoginovMV.Sprint4.Task7.V8.Lib;
+
+namespace Tyuiu.LoginovMV.Sprint4.Task7.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4| Выполнил: Логинов М.В | ИИПб-23-2";
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                                                  *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                                                    *");
+            Console.WriteLine("* Задание #7                                                                                                 *");
+            Console.WriteLine("* Вариант #8                                                                                                 *");
+            Console.WriteLine("* Выполнил: Логинов М.В   | ИИПб-23-2                                                                        *");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                   *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр 264795863157. Преобразуйте ее в матрицу 3 на 4                           *");
+            Console.WriteLine("* и подсчитайте сумму нечетных чисел.                                                                        *");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                           *");
+            Console.WriteLine("**************************************************************************************************************");
+            int n = 3;
+            int m = 4;
+            int index = 0;
+            string str = "264795863157";
+            int[,] array = new int[n, m];
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* Результат:                                                                                                 *");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("Сумма нечетных элементов массива: ");
+            int res = ds.Calculate(n, m, str);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
